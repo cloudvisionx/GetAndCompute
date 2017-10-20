00b968ca9c0d86933187b9b10e40e6c7cbe308cc
oraclex <longhunx@qq.com>
2017-10-20 18:54:31 +0800

    增加计算比例

--- GetAndComputeFromYahoo.py
+++ GetAndComputeFromYahoo.py
@@ -2,19 +2,13 @@
 # coding=utf-8
 import ystockquote
 import pandas as pd
+import math
 
 # print(ystockquote.get_shares_outstanding('GOOGL+AAPL'))
 # print(ystockquote.get_shares_outstanding('AAPL'))
 # print(ystockquote.get_float_shares('AAPL'))
 
-stock_dict = {
-    'symbol': [],
-    'close': [],
-    'shares_outstanding': [],
-    'shares_float': [],
-    'shares_ratio': [],
-    'weight': [],
-}
+
 
 symbol_list = ['0700.hk', 'BABA']
 
@@ -27,7 +21,7 @@ def main():
 
 def initilize_data():
     df = pd.DataFrame(index=symbol_list,
-                      columns={'close', 'shares_outstanding', 'shares_float', 'shares_ratio', 'weight'})
+                      columns={'price', 'shares_outstanding', 'shares_float', 'ratio', 'shares_ratio', 'weight'})
     # df['symbol'] = symbol_list
     return df
 
@@ -38,14 +32,44 @@ def get_shares_outstanding(df):
     for index, row in df.iterrows():
         symbol_str += index
         symbol_str += '+'
-    final_symbol_str=symbol_str[0:len(symbol_str)-1]
+    final_symbol_str = symbol_str[0:len(symbol_str) - 1]
     shares_oustanding = ystockquote.get_shares_outstanding(final_symbol_str)
-    df['shares_outstanding'] = shares_oustanding.encode('utf-8').split('\n')
+    shares_float = ystockquote.get_float_shares(final_symbol_str)
+    # shares_oustanding = ystockquote.get_bid_realtime(final_symbol_str)
+    shares_price = ystockquote.get_previous_close(final_symbol_str)
+    df['shares_outstanding'] = map(int,shares_oustanding.encode('utf-8').split('\n'))
+    df['shares_float'] = map(int,shares_float.encode('utf-8').split('\n'))
+    df['price'] = map(float,shares_price.encode('utf-8').split('\n'))
+    calRatio(df)
     print df
 
 
 
 
+
+def calRatio(df):
+    for index, row in df.iterrows():
+        tempRatio = row['shares_float'] / row['shares_outstanding']
+        final_ratio = 0
+        if (tempRatio <= 15):
+            final_ratio = math.ceil(tempRatio)
+        elif (tempRatio <= 20):
+            final_ratio = 20
+        elif (tempRatio <= 30):
+            final_ratio = 30
+        elif (tempRatio <= 40):
+            final_ratio = 40
+        elif (tempRatio <= 50):
+            final_ratio = 50
+        elif (tempRatio <= 60):
+            final_ratio = 60
+        elif (tempRatio <= 70):
+            final_ratio = 70
+        elif (tempRatio <= 80):
+            final_ratio = 80
+        else:
+            final_ratio = 100
+        row['ratio']=final_ratio
 
 
 if __name__ == "__main__":
